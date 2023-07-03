fd1a3b285fb29c9a3190f1a4d49508d4c34185d3
JGBeavers <67471068+JGBeavers@users.noreply.github.com>
2023-07-03 11:04:32 -0400

    Add files via upload

---
+++ 10-2-exercise/app.js
@@ -0,0 +1,7 @@
+// var PI = 3.14;
+// PI = 42; // stop me from doing this!
+
+const PI = 3.14;
+PI = 42; // unable to reassign due to const being declared
+
+// Not sure what Springboard wants with these refactoring code exercises..
